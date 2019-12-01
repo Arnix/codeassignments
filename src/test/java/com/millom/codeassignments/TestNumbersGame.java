f01875ae1cb6c1b3306f9dd30f6435b29fd3b0e7
AnnaOwsianka <anna.owsianka1@gmail.com>
2019-12-01 19:41:50 +0100

    implementation changes for Test

--- src/test/java/com/millom/codeassignments/TestNumbersGame.java
+++ src/test/java/com/millom/codeassignments/TestNumbersGame.java
@@ -1,7 +1,15 @@
 package com.millom.codeassignments;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,59 +18,66 @@ public class TestNumbersGame {
 
   private NumbersGame numbersGame;
   private static final Integer[] NUMBERS = {3, 9, 50, 15, 99, 7, 98, 65};
+  private CountDownLatch latch = new CountDownLatch(0);
 
   @Before
   public void setup() {
     numbersGame = new NumbersGame(NUMBERS);
   }
 
+  @After
+  public void end() throws InterruptedException {
+      latch.await(500L, TimeUnit.MILLISECONDS);
+  }
   @Test
   public void testClosestNumbers() {
     // todo: set correct expectation
-    int expectClosest = 88;
+    int expectClosest = 1; //between 98 and 99
     Assert.assertEquals(expectClosest, numbersGame.closestNumbers());
   }
 
   @Test
   public void testSortedNumbers() {
-    List<Integer> sorted = numbersGame.getSortedNumbers();
-
-    // todo: implement test to verify result
+      Arrays.sort(NUMBERS);
+      ArrayList<Integer> sortedNumbers = new ArrayList<>(Arrays.asList(NUMBERS));
+      Assert.assertEquals(numbersGame.getSortedNumbers(), sortedNumbers);
   }
 
   @Test
   public void testSum() {
     // todo: set correct expectation
-    long expectedSum = 88;
+    long expectedSum = 346;
     Assert.assertEquals(expectedSum, numbersGame.sum());
   }
 
   @Test
   public void testSumOfOdds() {
     // todo: set correct expectation
-    long expectedSum = 33;
+    long expectedSum = 198L;
     Assert.assertEquals(expectedSum, numbersGame.sumOfOdds());
   }
 
   @Test
   public void testSumOEvens() {
     // todo: set correct expectation
-    long expectedSum = 44;
+    long expectedSum = 148L;
     Assert.assertEquals(expectedSum, numbersGame.sumOfEvens());
   }
 
   @Test
   public void testSumOfEvensAndOddsEqualsSum() {
-    // todo: implement a test that shows that "sumOfEvens() + sumOfOdds == sum()"
+    Assert.assertEquals(numbersGame.sumOfEvens() + numbersGame.sumOfOdds(), numbersGame.sum());
+
   }
 
   @Test
   public void testAsyncSum() throws ExecutionException, InterruptedException {
     // todo: set correct expectation
-    Long expectedSum = 100L;
-    // todo: implement the assertion of the result
-    // Implementation hint: look at #CompletionStage.thenAccept
+    Long expectedSum = 346L;
+    CompletableFuture<Long> longCompletableFuture = numbersGame.sumAsync().toCompletableFuture();
 
+    longCompletableFuture.thenAccept(a -> Assert.assertEquals(expectedSum,a)).join();
+    longCompletableFuture.get();
   }
 
   /*
@@ -73,9 +88,16 @@ public class TestNumbersGame {
 
   @Test
   public void testAsyncSumOfOdds() throws ExecutionException, InterruptedException {
-    // todo: set correct expectation
-    Long expectedSum = 88L;
-    // todo: implement the assertion of the result
+    Long expectedSum = 198L;
+    latch = new CountDownLatch(1);
+    CompletableFuture<Long> longCompletableFuture = numbersGame.sumOfOddsAsync().toCompletableFuture();
+
+    longCompletableFuture.thenAccept(a ->{
+      Assert.assertEquals(expectedSum,a);
+      latch.countDown();
+    }).join();
+
+    longCompletableFuture.get();
     // Implementation hint: look at #CompletionStage.thenAccept
     // Also remember to wait for the completion of the test before continuing the test
     // for instance, use a CountdownLatch and wait for it outside
@@ -83,18 +105,29 @@ public class TestNumbersGame {
 
   @Test
   public void testAsyncSumOfEvens() throws ExecutionException, InterruptedException {
-    // todo: set correct expectation
-    Long expectedSum = 88L;
-    // todo: implement the assertion of the result
-    // Implementation hint: look at #CompletionStage.thenAccept
+    Long expectedSum = 148L;
+    latch = new CountDownLatch(1);
+    CompletableFuture<Long> longCompletableFuture = numbersGame.sumOfEvensAsync().toCompletableFuture();
+
+    longCompletableFuture.thenAccept(a ->{
+      Assert.assertEquals(expectedSum,a);
+      latch.countDown();
+    }).join();    // Implementation hint: look at #CompletionStage.thenAccept
     // Also remember to wait for the completion of the test before continuing the test
     // for instance, use a CountdownLatch and wait for it outside
   }
 
   @Test
-  public void testAsyncSumOfEvensAndOddsEqualsSum() {
+  public void testAsyncSumOfEvensAndOddsEqualsSum() throws ExecutionException, InterruptedException {
     // todo: implement a test that shows that "sumOfEvensAsync() + sumOfOddsAsync == sumAsync()"
     // todo: implement the assertion of the result
+    latch = new CountDownLatch(1);
+    CompletableFuture<Long> sumOfEvensCompletableFuture = numbersGame.sumOfEvensAsync().toCompletableFuture();
+    CompletableFuture<Long> sumOfOddsCompletableFuture = numbersGame.sumOfOddsAsync().toCompletableFuture();
+    CompletableFuture<Long> sumOfAllCompletableFuture = numbersGame.sumAsync().toCompletableFuture();
+    Assert.assertEquals(sumOfEvensCompletableFuture.get() + sumOfOddsCompletableFuture.get(), sumOfAllCompletableFuture.get().longValue();
+
+    latch.countDown();
     // Implementation hint: see if you can combine the previous three methods in a smart way using
     // the completion stages
     // Also remember to wait for the completion of the test before continuing the test
